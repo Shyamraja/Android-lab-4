5f000fc239991f01f5f31da513d05b6169bc760c
Shyamraja <t7bhsh00@students.oamk.fi>
2019-04-29 20:06:33 +0300

    created manifest and changed some things in main activity and  main.xml file

---
+++ lab42/app/src/main/java/com/example/lab42/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lab42;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //final TextView textView = (TextView) findViewById(R.id.text);
+        EditText editText;
+        final TextView textView= findViewById(R.id.textView);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.toString());
+                    }
+                }, new Response.ErrorListener() {
+               @Override
+               public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
